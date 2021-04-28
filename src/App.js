e0b9661e67f05ba8e6a0fdea70563e801a297fab
Lee Soim <hande.sof@gmail.com>
2021-04-28 13:33:37 +0900

    ADD default file and folder

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      {/* <div className="lds-container">
+        <div className="lds-ring">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>loading...</div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
