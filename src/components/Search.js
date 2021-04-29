13f1ceebfa0fbe62e0108dbabe0ca5e54a79e2d5
Lee Soim <hande.sof@gmail.com>
2021-04-29 22:45:01 +0900

    UPDATE components and pages code for link between pages

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 
 const Search = ({ handleButtonClick }) => {
-  const [ queryString, setQueryString ] = useState(null);
+  const [ queryString, setQueryString ] = useState("");
 
   const changeQueryString = (e) => {
     setQueryString(e.target.value);
   }
 
+  const getSearchResult = () => {
+    handleButtonClick(queryString);
+    setQueryString("");
+  }
+
   return (
     <div className="search-bar">
-      <input className="search-input" onChange={changeQueryString} type="text" />
-      <button className="search-btn" onClick={() => handleButtonClick(queryString)}>
+      <input className="search-input" onChange={changeQueryString} type="text" value={queryString}/>
+      <button className="search-btn" onClick={getSearchResult}>
         검색
       </button>
     </div>
