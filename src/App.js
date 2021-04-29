0814b540b745af838a45f162e97b2b3e64444cfe
halfkiwi90 <happykiwi90@gmail.com>
2021-04-29 17:32:30 +0900

    install eslint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import Login from './pages/login'
+import Signup from './pages/signup'
 
 function App() {
   return (
     <div className="App">
+      {/* <Login/> */}
+      <Signup/>
       hello world
       {/* <div className="lds-container">
         <div className="lds-ring">
