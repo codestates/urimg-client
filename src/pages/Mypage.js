4df2e4af4ffea56e8398490b8666987327286849
Lee Soim <hande.sof@gmail.com>
2021-05-01 16:56:20 +0900

    Add images type select and show user images in first loading

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import ImageList from "../components/ImageList";
 
+import { imagesData } from "../fakeData/images";
+
 const Mypage = ({ userInfo }) => {
+  const [ images, setImages ] = useState([]);
+
   if (JSON.stringify(userInfo) === '{}') {
     userInfo = {
-      "id": "id",
+      "id": "8LaX7CyhXx4",
       "user_name": "fake name",
       "email": "fake@email.com",
       "profile_image": "default-profile_640.png",
     }
+  }
+
+  useEffect(() => getUserImages(), [])
+
+  const getUserImages = () => {
+    // 서버에 요청해서 유저가 올린 이미지 데이터 리스트를 받아오는 함수 작성 예정
+    const userImages = imagesData.filter((image) => image.user.id === userInfo.id );
+    setImages(userImages);
+  }
+
+  const getLikedImages = () => {
+    // 서버에 요청해서 유저가 좋아요 표시한 이미지 데이터 리스트를 받아오는 함수 작성 예정
+    setImages(imagesData);
   }
 
   return (
@@ -25,6 +42,13 @@ const Mypage = ({ userInfo }) => {
             <button>회원정보 수정</button>
           </div>
         </div>
+        <div className="image-list-type">
+          <ul>
+            <li onClick={getUserImages}>Photo</li>
+            <li onClick={getLikedImages}>Like</li>
+          </ul>
+        </div>
+        <ImageList images={images} />
       </div>
     )
   }
